9e216d9ab66603b45e6f873b4add4d3c076a036a
zhaowei <choui20016514@gmail.com>
2022-07-21 14:58:49 +0800

    🚀  deploy to gitpages

--- src/sdk/util.ts
+++ src/sdk/util.ts
@@ -25,7 +25,7 @@ export function randomNumber(len: number): number {
 export function isPc(): boolean {
   const p = navigator.platform;
   let system = { win: p.indexOf("Win") === 0, mac: p.indexOf("Mac") === 0 };
-  if (process.env.REACT_APP_MOBILE === "yes") {
+  if (process && process.env && process.env.REACT_APP_MOBILE === "yes") {
     return false;
   }
   return system.win || system.mac;
@@ -83,9 +83,7 @@ export function getUrlParams(url: string) {
   return result;
 }
 
-export function getVideoResolution(
-  level: string
-): {
+export function getVideoResolution(level: string): {
   width: number;
   height: number;
   bitrate: number;
