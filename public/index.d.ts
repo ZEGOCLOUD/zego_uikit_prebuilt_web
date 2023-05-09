24856cac66a16dcf44405e2b9477d715cc63c36e
leanwang <leanwang@zego.im>
2023-05-09 19:13:21 +0800

    build:1.8.2

--- public/index.d.ts
+++ public/index.d.ts
@@ -128,7 +128,8 @@ declare interface ZegoCloudRoomConfig {
   showInviteToCohostButton?: boolean; // Whether to show the button that is used to invite the audience to co-host on the host end.
   showRemoveCohostButton?: boolean; // Whether to show the button that is used to remove the audience on the host end.
   showRequestToCohostButton?: boolean; // Whether to show the button that is used to request to co-host on the audience end.
-  rightPanelExpandedType?: RightPanelExpandedType; // The right panel is expanded， The default value is "None".
+  rightPanelExpandedType?: RightPanelExpandedType; // Controls the type of the information displayed on the right panel, display "None" by default.
+
   // 1.4 Leaving view
   showLeavingView?: boolean; // Whether to display the leaving view. Displayed by default.
 
